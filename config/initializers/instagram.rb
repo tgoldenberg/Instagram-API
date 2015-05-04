d5bdaaca0c14fe5c4718239b58759e243a8d9202
tgoldenberg <thomasagoldenberg@gmail.com>
2015-05-04 14:53:01 -0400

    add title header

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,3 +1,3 @@
-access_token =  2053654095.8fd8872.8110806e6b3f4bedbd2e58ec28ed2f9a
-client_id = 8fd88729ccb74495ae9aaad32606fe7d
-client_secret = 62dab21801bf486389ca7ce95b4effdb
+# access_token =  2053654095.8fd8872.8110806e6b3f4bedbd2e58ec28ed2f9a
+# client_id = 8fd88729ccb74495ae9aaad32606fe7d
+# client_secret = 62dab21801bf486389ca7ce95b4effdb
