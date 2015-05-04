42b1495abc824d8041c1ab34d6fb33837c3622f3
tgoldenberg <thomasagoldenberg@gmail.com>
2015-05-04 15:59:15 -0400

    get access to user's instagram photo hash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,15 +15,50 @@
 //= require turbolinks
 //= require_tree .
 $(document).ready(function() {
-var menuToggle = $('#js-mobile-menu').unbind();
-$('#js-navigation-menu').removeClass("show");
 
-menuToggle.on('click', function(e) {
-  e.preventDefault();
-  $('#js-navigation-menu').slideToggle(function(){
-    if($('#js-navigation-menu').is(':hidden')) {
-      $('#js-navigation-menu').removeAttr('style');
+  $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=2053654095.8fd8872.8110806e6b3f4bedbd2e58ec28ed2f9a",
+        success: function(data) {
+          console.log(data);
+          for (var i = 0; i < 10; i++) {
+          $('.pics').append("<a target='_blank' href='" + data.data[i].link +
+          "'><img src='" + data.data[i].images.low_resolution.url +"'></img></a>");
+          }
+        }
+    });
+
+
+
+
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs');
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
     }
   });
-});
 });
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,3 +1,12 @@
 # access_token =  2053654095.8fd8872.8110806e6b3f4bedbd2e58ec28ed2f9a
 # client_id = 8fd88729ccb74495ae9aaad32606fe7d
 # client_secret = 62dab21801bf486389ca7ce95b4effdb
+require "instagram"
+
+  Instagram.configure do |config|
+
+  config.client_id = "8fd88729ccb74495ae9aaad32606fe7d"
+
+  config.access_token = "2053654095.8fd8872.8110806e6b3f4bedbd2e58ec28ed2f9a"
+  
+  end
